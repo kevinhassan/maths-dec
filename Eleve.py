e1739c91ca1eecb64e9566895f7f7a58a9682e86
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-12-02 11:07:22 +0100

    [add] Eleve structure class

---
+++ Eleve.py
@@ -0,0 +1,14 @@
+class Eleve:
+    def __init__(self, nom, prenom, notes):
+        self.nom = nom
+        self.prenom = prenom
+        self.notes = notes
+
+    def nom(self):
+        return self.nom
+
+    def prenom(self):
+        return self.prenom
+
+    def notes(self):
+        return self.notes
