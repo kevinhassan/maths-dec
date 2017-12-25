8672e1c3021c3155042eac8c666aea4cb5bf39e4
kevinhassan <kevin.hassan@etu.umontpellier.fr>
2017-12-25 22:31:42 +0100

    add: function permutation entre 2 groupes

--- algo.py
+++ algo.py
@@ -22,13 +22,13 @@ def testNbGroupes(groupes, nbBinome, nbTrinome):
     Premier élément : groupe dont la satisfaction est la plus basse
 """
 def trierGroupe(groupes):
-    groupes.sort(key=lambda x: x.getNote())
+    groupes.sort(key=lambda x: min(x.getNote()))
 
 """
     Prendre le groupe dont la note est la plus basse
 """
 def getMinGroupe(groupes):
-    return min(groupes, key=lambda x: x.getNote())
+    return min(groupes, key=lambda x: min(x.getNote()))
 
 """
     Récupérer la matrice de préférence
@@ -84,3 +84,28 @@ for i in range(nbBinome*2, nbBinome*2 + nbTrinome*3):
         trinome = []
 
 print(testNbGroupes(groupes, nbBinome, nbTrinome))
+
+"""
+    Fonction qui permute 2 élèves dont les satisfactions sont les plus basse 
+"""
+def permutation(groupes,i,j):
+    groupe1 = groupes[i]
+    groupe2 = groupes[j]
+    e1 = groupe1.getMoinsSatisfait()
+    e2 = groupe2.getMoinsSatisfait()
+    groupe1.supprimerEleve(e1)
+    groupe2.supprimerEleve(e2)
+    groupe1.ajouterEleve(e2)
+    groupe2.ajouterEleve(e1)
+    groupes[i] = groupe1
+    groupes[j] = groupe2
+    return groupes
+"""
+    Réaliser les permutations de la manière suivante : 
+    * prendre la mention la plus basse des groupes formés
+    * tout groupe de mention égale doivent être interverti avec les autres groupes
+    * la permutation est valable si : 
+        - la satisfaction du groupe choisi est améliorée et si la satisfaction de l'autre groupe n'est pas en dessous du seuil
+    * la permutation n'a plus d'intêret dès lors que toutes les permutations réalisées sur un groupe ne l'améliore pas 
+"""
+
